fd176558e8a00e2cda68b7604da62bc55664c343
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2024-06-04 16:45:24 +0200

    WordPairStorage.pairsToStrings() WordPairStorage.stringsToPairs()

--- lib/storage.dart
+++ lib/storage.dart
@@ -11,4 +11,25 @@ abstract class WordPairStorage {
   Future<(List<WordPair>, Set<WordPair>)> load();
 
   factory WordPairStorage(String name) => getWordPairStorageImpl(name);
+
+  static List<String> pairsToStrings(Iterable<WordPair> list, Set<WordPair> deleted) {
+    var strings = list.map((pair) {
+      return "${pair.first} ${pair.second}${deleted.contains(pair) ? ' *' : ''}";
+    });
+    return strings.toList();
+  }
+
+  static (List<WordPair>, Set<WordPair>) stringsToPairs(Iterable<String> strings) {
+    List<WordPair> pairs = <WordPair>[];
+    Set<WordPair> deleted = <WordPair>{};
+    for (final string in strings) {
+      final words = string.split(' ');
+      final pair = WordPair(words[0], words[1]);
+      pairs.add(pair);
+      if (words.length > 2 && words[2] == '*') {
+        deleted.add(pair);
+      }
+    }
+    return (pairs, deleted);
+  }
 }
--- lib/storage_impl/path_provider.dart
+++ lib/storage_impl/path_provider.dart
@@ -36,8 +36,7 @@ class PathProviderWordPairStorage implements WordPairStorage {
 
   @override
   Future<void> save(Iterable<WordPair> list, Set<WordPair> deleted) async {
-    final contents =
-        list.map((pair) => "${pair.first} ${pair.second}${deleted.contains(pair) ? ' *' : ''}").join('\n');
+    final contents = WordPairStorage.pairsToStrings(list, deleted).join('\n');
     final f = await file;
     f.writeAsString(contents);
     log.i("Saved ${list.length} word pairs to $f, of which ${deleted.length} are deleted");
@@ -46,20 +45,13 @@ class PathProviderWordPairStorage implements WordPairStorage {
   @override
   Future<(List<WordPair>, Set<WordPair>)> load() async {
     final f = await file;
-    final List<WordPair> list = <WordPair>[];
-    final Set<WordPair> deleted = <WordPair>{};
-    if (await f.exists()) {
-      final contents = await f.readAsString();
-      final lines = contents.split('\n');
-      for (final line in lines) {
-        final words = line.split(' ');
-        final pair = WordPair(words[0], words[1]);
-        list.add(pair);
-        if (words.length > 2 && words[2] == '*') {
-          deleted.add(pair);
-        }
-      }
+    if (!await f.exists()) {
+      log.e("$f does not exist");
+      return (<WordPair>[], <WordPair>{});
     }
+    final contents = await f.readAsString();
+    final lines = contents.split('\n');
+    final (list, deleted) = WordPairStorage.stringsToPairs(lines);
     log.i("Loaded ${list.length} word pairs and ${deleted.length} deleted word pairs from $f");
     return (list, deleted);
   }
