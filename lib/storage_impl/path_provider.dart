36693572ee9070f0e401cad6860870a70f2b0b64
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2024-06-05 11:53:18 +0200

    rewrite PathProviderWordPairStorage using just final Future<File>

--- lib/storage_impl/path_provider.dart
+++ lib/storage_impl/path_provider.dart
@@ -9,48 +9,41 @@ import '../storage.dart';
 WordPairStorage getWordPairStorageImpl(String name) => PathProviderWordPairStorage(name);
 
 class PathProviderWordPairStorage implements WordPairStorage {
-  PathProviderWordPairStorage(String name) : _filename = '$name.txt' {
-    log.i("Created PathProviderWordPairStorage for $_filename");
+  PathProviderWordPairStorage(String name) : _file = _createFileIfNotExists('$name.txt') {
+    _file.then((file) => log.i("PathProviderWordPairStorage is ready for $file"));
   }
 
-  String _filename;
-  String? _directoryPath;
+  final Future<File> _file;
 
-  String get filename => _filename;
-
-  Future<String> get directoryPath async {
-    if (_directoryPath == null) {
-      final dir = await path_provider.getApplicationDocumentsDirectory();
-      _directoryPath = dir.path;
+  static Future<File> _createFileIfNotExists(String filename) async {
+    final dir = await path_provider.getApplicationDocumentsDirectory();
+    final filePath = '${dir.path}/$filename';
+    var f = File(filePath);
+    if (!await f.exists()) {
+      f = await f.create();
+      log.i("Created new data file $f");
     }
-    return _directoryPath!;
-  }
-
-  Future<String> get filePath async {
-    return '${await directoryPath}/$_filename';
-  }
-
-  Future<File> get file async {
-    return File(await filePath);
+    assert(await f.exists());
+    return f;
   }
 
   @override
   Future<void> save(Iterable<WordPair> list, Set<WordPair> deleted) async {
+    final f = await _file;
     final contents = WordPairStorage.pairsToStrings(list, deleted).join('\n');
-    final f = await file;
     f.writeAsString(contents);
     log.i("Saved ${list.length} word pairs to $f, of which ${deleted.length} are deleted");
   }
 
   @override
   Future<(List<WordPair>, Set<WordPair>)> load() async {
-    final f = await file;
+    final f = await _file;
     if (!await f.exists()) {
       log.e("$f does not exist");
       return (<WordPair>[], <WordPair>{});
     }
     final contents = await f.readAsString();
-    final lines = contents.split('\n');
+    final lines = contents.isNotEmpty ? contents.split('\n') : <String>[];
     final (list, deleted) = WordPairStorage.stringsToPairs(lines);
     log.i("Loaded ${list.length} word pairs and ${deleted.length} deleted word pairs from $f");
     return (list, deleted);
